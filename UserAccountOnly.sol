2b7ddf506fef608198217242d714a85baf57b5b8
Jhbruton <57003500+Jhbruton@users.noreply.github.com>
2020-02-26 19:26:31 -0600

    Update UserAccountOnly.sol

--- UserAccountOnly.sol
+++ UserAccountOnly.sol
@@ -1,15 +1,12 @@
-
-pragma solidity ^0.4.18;pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 <0.7.0;
 
 contract MappedStructsWithIndex {
-    
     enum UserType {Creator, Worker, Abitrator}
     enum UserTier {TierOne, TierTwo, TierThree}
     enum AccountStatus {good, restricted}
     uint256 public tasksCompleted;
     bool isOpen = true;
 
-    
     struct UserAccount {
         bool isOpen;
         string userName;
@@ -18,34 +15,31 @@ contract MappedStructsWithIndex {
         uint256 tasksCompleted;
         AccountStatus accountStatus;
     }
-    
-  struct EntityStruct {
-    uint entityData;
-    bool isEntity;
-  }
+
+    struct EntityStruct {
+        uint256 entityData;
+        bool isEntity;
+    }
 
     mapping(address => UserAccount) public userStructs;
     address[] public userLists;
-  
 
-    function isUser(address userAddress) public view returns(string userName){
+    function isUser(address userAddress) public view returns (string userName) {
         return userStructs[userAddress].userName;
     }
-    
-    function getUserCount() public view returns(uint userCount) {
+
+    function getUserCount() public view returns (uint256 userCount) {
         return userLists.length;
     }
-  
-  function newUser(string userName) public returns(uint rowNumber){
-     require(!userStructs[msg.sender].isOpen);
-      userStructs[msg.sender].userName = userName;
-      userStructs[msg.sender].isOpen = false;
-      return userLists.push(msg.sender) -1;
-  }
-      
-  
 
-/*
+    function newUser(string userName) public returns (uint256 rowNumber) {
+        require(!userStructs[msg.sender].isOpen);
+        userStructs[msg.sender].userName = userName;
+        userStructs[msg.sender].isOpen = false;
+        return userLists.push(msg.sender) - 1;
+    }
+
+    /*
   function newEntity(address entityAddress, uint entityData) public returns(uint rowNumber) {
     if(isEntity(entityAddress)) revert();
     entityStructs[entityAddress].entityData = entityData;
