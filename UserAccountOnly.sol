16a45c15c042895f9b1a77046647f3a339fb7bb4
Jhbruton <57003500+Jhbruton@users.noreply.github.com>
2020-03-09 19:47:51 -0500

    Update UserAccountOnly.sol
    Added new Admin Functions, restrictions depending on account status

--- UserAccountOnly.sol
+++ UserAccountOnly.sol
@@ -1,30 +1,50 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract MappedStructsWithIndex {
-    enum UserType {Creator, Worker}
+    enum UserType {Creator, Worker, Arbitrator, Admin}
     enum UserTier {TierOne, TierTwo, TierThree}
     enum AccountStatus {good, restricted}
     uint256 public tasksCompleted;
-    bool isOpen = true;
+    address public RootAdmin;
+    
+    constructor() public {
+        RootAdmin = msg.sender;
+    }
+    
+    
+    modifier onlyRootAdmin (){
+    require(msg.sender == RootAdmin, "Only Rootadmins may perform this function");
+            _;
+    }
+    
+    modifier onlyArbitrator(){
+        require(userStructs[msg.sender].userType == UserType.Arbitrator, "Only Arbitrators may perform this function");
+        _;
+    }
+    
+    modifier goodStatus() {
+        require(userStructs[msg.sender].accountStatus == AccountStatus.good, "Your account is not in good standing");
+        _;
+    }
+    
+    event NewUserRegistered (address userAddress);
+    event UserTierUpgrade (UserTier);
 
     struct UserAccount {
-        bool isOpen;
+        bool isUsed;
         string userName;
         UserTier userTier;
         UserType userType;
         uint256 tasksCompleted;
         AccountStatus accountStatus;
-        bool isArbitrator;
-        
     }
 
-    struct EntityStruct {
-        uint256 entityData;
-        bool isEntity;
-    }
 
     mapping(address => UserAccount) public userStructs;
     address[] public userLists;
+    
+   
+
 
     function isUser(address userAddress) public view returns (string memory userName) {
         return userStructs[userAddress].userName;
@@ -34,14 +54,11 @@ contract MappedStructsWithIndex {
         return userLists.length;
     }
 
-    function newUser(string memory userName, UserType _UserType) public returns (uint256 rowNumber) {
-        require (userStructs[msg.sender].isOpen == false);
+    function newUser(string memory userName) public returns (uint256 rowNumber) {
+        require(userStructs[msg.sender].isUsed != true);
         userStructs[msg.sender].userName = userName;
-        userStructs[msg.sender].isOpen = true;
-        userStructs[msg.sender].userType = _UserType;
-        userStructs[msg.sender].isArbitrator == false;
+        userStructs[msg.sender].isUsed = true;
         return userLists.push(msg.sender) - 1;
-    
     }
     
    function updateUserTier() public returns(bool success) {
@@ -57,29 +74,37 @@ contract MappedStructsWithIndex {
       return true;
         }
    
-   function appointArbitrator() public {
+   function appointArbitrator() public goodStatus returns(bool success) {
        require (userStructs[msg.sender].userTier == UserTier.TierThree, 'You must be a TierThree user to arbitrate');
-       userStructs[msg.sender].isArbitrator = true;
+       userStructs[msg.sender].userType = UserType.Arbitrator;
+       return true;
    }
    
-   function updateUserComlete(uint _tasksCompleted) public {
+   function updateUserComplete(uint _tasksCompleted) public {
        userStructs[msg.sender].tasksCompleted += _tasksCompleted;
    }
    
+   function createAdmin(address _address) public onlyRootAdmin {
+       userStructs[_address].userType = UserType.Admin;
+   }
+       
+    function demoteAdmin(address _address, uint _Index) public onlyRootAdmin {
+        userStructs[_address].userType = UserType(_Index);
+    }
+   
+   
+   function restrictAccount(address _address) public {
+       require (userStructs[msg.sender].userType == UserType.Admin, "Only Admins may perform this function");
+       userStructs[_address].accountStatus = AccountStatus.restricted;
+   }
+   
+   function restoreAccount(address _address) public {
+       require (userStructs[msg.sender].userType == UserType.Admin, "Only Admins may perform this function");
+       userStructs[_address].accountStatus = AccountStatus.restricted;
+       
+   }
+   
+}
    
 
-    /*
-  function newEntity(address entityAddress, uint entityData) public returns(uint rowNumber) {
-    if(isEntity(entityAddress)) revert();
-    entityStructs[entityAddress].entityData = entityData;
-    entityStructs[entityAddress].isEntity = true;
-    return entityList.push(entityAddress) - 1;
-  
-
-  function updateEntity(address entityAddress, uint entityData) public returns(bool success) {
-    if(!isEntity(entityAddress)) revert();
-    entityStructs[entityAddress].entityData    = entityData;
-    return true
-    */
-
-}
+    
