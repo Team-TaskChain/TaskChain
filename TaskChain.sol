b3ab7c2b793ccc757ca2c15cc9125450e273019e
Jhbruton <57003500+Jhbruton@users.noreply.github.com>
2020-03-26 13:22:57 -0500

    Update TaskChain.sol

--- TaskChain.sol
+++ TaskChain.sol
@@ -46,8 +46,8 @@ contract TaskCreate {
         _;
     }
     
-    event NewUserRegistered (address userAddress);
-    event UserTierUpgrade (UserTier);
+    event NewUserRegistered (address indexed _from, string _username, UserType _userType);
+    event UserTierUpgrade (address indexed _from, string _success);
 
     //the base structure for all user accouts. isUsed is to restrict users to one account per address, which helps presever status and rank
     struct UserAccount {
@@ -88,28 +88,36 @@ contract TaskCreate {
         userStructs[msg.sender].userType = UserType(_index);
         userStructs[msg.sender].userName = userName;
         userStructs[msg.sender].isUsed = true;
-        userStructs[msg.sender].isAdmin = false;        
+        userStructs[msg.sender].isAdmin = false; 
+        emit NewUserRegistered(msg.sender, userName, UserType(_index));
         return userLists.push(msg.sender) - 1;
+      
     }
     
     //enables the uprage of tiers, depending on how many tasks have been completed
-   function updateUserTier() public returns(bool success) {
+   function updateUserTier() public goodStatus returns(string memory _success) {
        if (userStructs[msg.sender].tasksCompleted > 50 ) {
            userStructs[msg.sender].userTier =  UserTier.TierThree;
+           _success = "TierThree";
        }
        else if (userStructs[msg.sender].tasksCompleted > 10 && userStructs[msg.sender].tasksCompleted<50) {
            userStructs[msg.sender].userTier = UserTier.TierTwo;
+           _success = "TierTwo";
+
        }
        else {
             userStructs[msg.sender].userTier = UserTier.TierOne;
+            _success = "TierOne";
        }
-      return true;
+      emit UserTierUpgrade(msg.sender, _success);
+      return _success;
         }
-   
+   event appointedArbitrator(address indexed _from);
    //appoints a new arbitrator, must be tier three to perform this function
    function appointArbitrator() public goodStatus returns(bool success) {
        require (userStructs[msg.sender].userTier == UserTier.TierThree, 'You must be a TierThree user to arbitrate');
        userStructs[msg.sender].userType = UserType.Arbitrator;
+       emit appointedArbitrator(msg.sender);
        return true;
    }
    
@@ -118,24 +126,34 @@ contract TaskCreate {
        userStructs[msg.sender].tasksCompleted += _tasksCompleted;
    }
    
+   event newAdmin(address indexed _from, address indexed _to);
+   event removeAdmin(address indexed _from, address indexed _to);
+   event newRestrictedAccount(address indexed _from, address indexed _to);
+   event removeRestrictAccount(address indexed _from, address indexed _to);
+   
    //apoints new admin
    function createAdmin(address _address) public onlyRootAdmin {
        userStructs[_address].isAdmin = true;
+       emit newAdmin(msg.sender, _address);
    }
+   
    
    //removes admin, to prevent malicious users    
     function demoteAdmin(address _address) public onlyRootAdmin {
         userStructs[_address].isAdmin = false;
+        emit removeAdmin(msg.sender, _address);
     }
    
    //enables the restriction of accounts, for malicious use
    function restrictAccount(address _address) onlyAdmin public {
          userStructs[_address].accountStatus = AccountStatus.restricted;
+         emit newRestrictedAccount(msg.sender, _address);
    }
 
     //restores accounts, based on admin discretion. Timeouts will be added later, for automatic use by user
    function restoreAccount(address _address) onlyAdmin public {
        userStructs[_address].accountStatus = AccountStatus.restricted;
+       emit removeRestrictAccount(msg.sender, _address);
        
    }
    
@@ -147,9 +165,9 @@ contract TaskCreate {
   
   
   
-  event ContractCreated(address Creator, uint value);
-  event ContractClosed(address Creator, uint payout);
-  event workDone(address Worker, uint balanceContract);
+  event ContractCreated(address indexed _from, uint256 _value, uint256 _payout, UserTier _usertier);
+  event ContractClosed(address indexed _from, uint _bal);
+  event workDone(address indexed _from, address indexed _to, uint256 payout);
   
   // defines what tier the contract is. This will correlate to the minimum tier of user required to do work
   enum ContractTier {cTierOne, cTierTwo, cTierThree}
@@ -191,6 +209,7 @@ contract TaskCreate {
 function createContract(uint _taskTier, uint256 _quota, uint amount) public onlyCreators payable {
     uint amountEscrow = amount*5 /100;
     uint contractAmount = amount * 95/100;
+    uint contractPayout = contractAmount/_quota;
     contractStruct[msg.sender].ContractOwner = msg.sender;
     contractStruct[msg.sender].value = contractAmount;
     contractStruct[msg.sender].quota = _quota;
@@ -198,8 +217,9 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
     reserveWallet[RootAdmin]+=amountEscrow;
     contractBalance[msg.sender] += contractAmount;
     contractStruct[msg.sender].activeContract = true;
-    contractStruct[msg.sender].payout = contractAmount/_quota;
+    contractStruct[msg.sender].payout = contractPayout;
     require(msg.value == amount, "value mismatch!");
+    emit ContractCreated(msg.sender, contractAmount, contractPayout, UserTier(_taskTier));
 }
   
   event callArbitration(address indexed _from, address indexed _to, bool _passFail);
@@ -222,7 +242,7 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
 	     contractBalance[_add] -= contractStruct[_add].payout;
 	     addTaskComplete();
 	     workerPass[msg.sender] = false;
-	     emit workDone(msg.sender, contractBalance[_add]);
+	     emit workDone(_add, msg.sender, contractStruct[_add].payout);
         }
         
     function reviewWork(bool _passFail, address _add) onlyCreators public {
@@ -235,8 +255,10 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
         }
     }
     
+    event arbitrationWorking(address indexed _from, address indexed _to, address indexed _contract, bool _arbitrateResult);
+    event transferEscrowOut(address indexed _from, uint256 _balance);
     
-    //working HERE
+    //Controls arbitration functions
     function arbitrateWork(bool _passFail, address _workerAdd, address _contractAdd ) onlyArbitrator public payable {
         require(workerPass[_workerAdd]==false, "The worker must not have passed to arbitrate");
         if (_passFail==true){
@@ -245,13 +267,15 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
             contractBalance[_contractAdd]-=2*contractStruct[_contractAdd].payout;
             workerEscrow[msg.sender]+=contractStruct[_contractAdd].payout;
             arbitrationWallet[msg.sender]+=2*contractStruct[_contractAdd].payout;
+            emit arbitrationWorking(msg.sender, _workerAdd, _contractAdd, _passFail);
             }
-        if (_passFail==false) {
+        else if (_passFail==false) {
             workerPass[_workerAdd]=false;
             reserveWallet[RootAdmin]-= 2 * contractStruct[_contractAdd].payout;
             workerEscrow[_workerAdd]-=contractStruct[_contractAdd].payout;
             arbitrationWallet[msg.sender]+=2*contractStruct[_contractAdd].payout;
             contractBalance[_contractAdd]+= contractStruct[_contractAdd].payout;
+            emit arbitrationWorking(msg.sender, _workerAdd, _contractAdd, _passFail);
         }
     }
         
@@ -261,14 +285,18 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
         actBal = workerEscrow[msg.sender];
         workerEscrow[msg.sender]-= actBal;
         workerWallet[msg.sender]+= actBal;
+        emit transferEscrowOut(msg.sender, actBal);
         
     }
+    event cashOut(address indexed _from, uint256 _bal);
+    
     //cashes out worker wallet to worker: TODO add arbitration restrictions
     function workCashOut() onlyWorkers public payable {
        uint256 accountBal;
        accountBal= workerWallet[msg.sender];
        workerWallet[msg.sender]-= accountBal;
        msg.sender.transfer(accountBal);
+       emit cashOut(msg.sender, accountBal);
     }
     
     function arbitratorCashOut() public onlyArbitrator payable {
@@ -276,6 +304,7 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
         accountBal = arbitrationWallet[msg.sender];
         arbitrationWallet[msg.sender]-=accountBal;
         msg.sender.transfer(accountBal);
+        emit cashOut(msg.sender, accountBal);
     }
     // closes the contract, and empties wallet back to creator
     function closeContract() onlyCreators public payable {
@@ -285,6 +314,7 @@ function createContract(uint _taskTier, uint256 _quota, uint amount) public only
         contractBalance[msg.sender]-=accountBal;
         contractStruct[msg.sender].activeContract = false;
         msg.sender.transfer(accountBal);
+        emit ContractClosed(msg.sender, accountBal);
     }
 
     //checks current balance for the contract wallet
